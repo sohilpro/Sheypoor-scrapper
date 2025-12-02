e98ac7272a34a9a1d6140daf1658135a64697627
sohilpro <sohil123dhh@gmail.com>
2025-12-02 20:36:35 +0330

    fix env chrome

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM ghcr.io/puppeteer/puppeteer:21.5.2
 
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
+    PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
+
 WORKDIR /app
 
 # کپی کردن فایل‌های پکیج
--- Sheypoor-THR-SMNAN/src/services/scraper.js
+++ Sheypoor-THR-SMNAN/src/services/scraper.js
@@ -33,7 +33,8 @@ class Scraper {
     if (!this.browser) {
       this.browser = await puppeteer.launch({
         headless: "new",
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
+        executablePath:
+          process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome",
         args: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
